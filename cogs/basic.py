9d35d0804cbae7594d971fb274a926aae892df2b
Chris Johnston <chris@thejohnstons.net>
2018-05-23 18:33:18 -0700

    remove extra commands from the basic command cog to reduce complexity

--- cogs/basic.py
+++ cogs/basic.py
@@ -12,42 +12,12 @@ class BasicCog:
         self.bot = bot
 
     # ping command
-    @commands.cooldown(1, 30, commands.BucketType.user)
     @commands.command()
     async def ping(self, ctx):
+        # replies back to the command context with the
+        # text "Pong!"
         await ctx.send("Pong!")
-
-    # echo command
-    # commands are given a name implicitly, but you can also define this explicitly as well
-    # the cooldown attribute allows you to limit how frequently the command can be used in
-    # a channel, a server, or by a specific user
-    @commands.cooldown(1, 60, commands.BucketType.user)
-    @commands.command(name='echo')
-    async def echo(self, ctx, *, message: str='_echo_'):
-        await ctx.send(ctx.author.mention + ' : ' + message)
-
-    # sqrt command
-    @commands.command(name='sqrt')
-    async def sqrt(self, ctx, *, num: int=0):
-        await ctx.send(_sqrt(num))
 
 # add this cog to the bot
 def setup(bot):
     bot.add_cog(BasicCog(bot))
-
-def _sqrt(num: float):
-    """
-    Example of how to include methods that can be easily tested
-    
-    >>> _sqrt(4)
-    'Sqrt(4) = 2.0'
-
-    """
-
-    return 'Sqrt(' + str(num) + ') = ' + str(math.sqrt(num))
-
-
-# optional, but helpful for testing via the shell
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
